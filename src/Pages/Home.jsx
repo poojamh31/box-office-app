c40e9ba89c444ea4c7253228391460acac9c6487
pooja <you@example.com>
2024-06-09 18:05:29 +0530

    fetch and display data from tvmaze api. create reusable function to fetch data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { searchForShows } from "../api/tvmaze";
 const Home = () => {
   const [searchStr, setSearchStr] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onSearchInputChange = (ev) => {
     setSearchStr(ev.target.value);
@@ -8,14 +11,27 @@ const Home = () => {
 
   const onSearch = async (ev) => {
     ev.preventDefault();
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
-    const body = await response.json();
-    console.log(body);
-    //https://api.tvmaze.com/search/shows?q=boys
-  };
 
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured : {apiDataError.message} </div>;
+    }
+    if (apiData) {
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
   return (
     <div>
       <form onSubmit={onSearch}>
@@ -23,6 +39,7 @@ const Home = () => {
 
         <button type="submit">Search</button>
       </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 };
---
+++ src/api/tvmaze.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://api.tvmaze.com";
+
+const apiGet = async (queryString) => {
+  //throw new Error("something bad happened");
+  const response = await fetch(`${BASE_URL}${queryString}`);
+
+  const body = await response.json();
+
+  return body;
+};
+
+export const searchForShows = (query) => apiGet(`/search/shows?q=${query}`);
+
+//const response = await fetch(
+//   `https://api.tvmaze.com/search/shows?q=${searchStr}`
+// );
+// const body = await response.json();
