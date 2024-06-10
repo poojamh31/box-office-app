8abd19ead8f4d8df982eda81ae83c1f761a03fa1
pooja <you@example.com>
2024-06-10 18:06:31 +0530

    fetch show data inside show component

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getShowByID } from '../api/tvmaze';
 const Show = () => {
   const { showId } = useParams();
-  return <div>Show Page for {showId}</div>;
+
+  const [showData, setShowdata] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowByID(showId);
+        setShowdata(data);
+      } catch (err) {
+        setShowError(err);
+      }
+    }
+    fetchData();
+  }, [showId]);
+
+  if (showError) {
+    return <div>We have an error : {showError.message}</div>;
+  }
+  if (showData) {
+    return <div>Got show data : {showData.name}</div>;
+  }
+  return <div>Data id loading</div>;
 };
 
 export default Show;
--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -13,6 +13,8 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 
+export const getShowByID = showId => apiGet(`/shows/${showId}`);
+
 //const response = await fetch(
 //   `https://api.tvmaze.com/search/shows?q=${searchStr}`
 // );
