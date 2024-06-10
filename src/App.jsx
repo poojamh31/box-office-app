486ac22a3115cb4894274662efb29e24df6c5dba
pooja <you@example.com>
2024-06-10 15:56:52 +0530

    created dynamic show page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
 import MainLayout from './components/MainLayout';
+import Show from './Pages/Show';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/starred" element={<Starred />} />
         </Route>
+        <Route path="/show/:showId" element={<Show />} />
         <Route path="*" element={<div>Not found</div>} />
 
         {/* <Route path="/" element={<App />}>
