bd3834c186c18027bd8446350a3e823561642cb2
pooja <you@example.com>
2024-06-11 19:03:11 +0530

    Styled entire application using styled component

--- src/Pages/Starred.jsx
+++ src/Pages/Starred.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getShowByIDs } from '../api/tvmaze';
 import { useStarredShows } from '../lib/useStarredShows';
 import ShowGrid from '../components/shows/ShowGrid';
+import { TextCenter } from '../components/common/TextCenter';
 
 const Starred = () => {
   const [starredShowsIds] = useStarredShows();
@@ -16,17 +17,17 @@ const Starred = () => {
   });
 
   if (starredShows?.length === 0) {
-    return <div>No shows were starred</div>;
+    return <TextCenter>No shows were starred</TextCenter>;
   }
 
   if (starredShows?.length > 0) {
     return <ShowGrid shows={starredShows} />;
   }
   if (starredShowsError) {
-    return <div>Error Occured: {starredShowsError.message}</div>;
+    return <TextCenter>Error Occured: {starredShowsError.message}</TextCenter>;
   }
 
-  return <div>Shows are loading</div>;
+  return <TextCenter>Shows are loading</TextCenter>;
 };
 
 export default Starred;
